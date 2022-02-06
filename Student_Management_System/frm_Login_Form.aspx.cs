85f4d2f30490668732861296618cd1e2b9aee243
snehal renushe <snehalrenushe15@gmail.com>
2022-02-06 22:53:43 +0530

    Login Form Added

---
+++ Student_Management_System/frm_Login_Form.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class frm_Login_Form : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        btn_Submit.Enabled = false;
+    }
+
+    protected void tb_Password_TextChanged(object sender, EventArgs e)
+    {
+        btn_Submit.Enabled = true;
+    }
+
+    protected void btn_Submit_Click(object sender, EventArgs e)
+    {
+        if (tb_Username.Text == "a" && tb_Password.Text == "a")
+        {
+            
+        }
+    }
+}
